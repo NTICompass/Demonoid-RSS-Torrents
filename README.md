393a298d7f3dcef1be72116bd04af132285c11b8
Rocket <NTICompass@gmail.com>
2011-05-22 18:58:28 -0400

    Here is demonoid RSS script I wrote for use in torrentflux.

---
+++ README.md
@@ -0,0 +1,27 @@
+## README ##
+
+This script gets a user's RSS feed from [demonoid][1] and changes the links to point to the actual torrent file instead of just the torrent page.
+
+It does this by changing the link to point to this script which will login to demonoid and get the torrent file.
+
+## Pre-Setup ##
+
+First things first, you must have a demonoid account for this to work.
+Your server will need to have PHP5, cURL, SimpleXML, and DOMDocument to use this script.
+
+## Setup ##
+
+To use this script, place it on your webserver somewhere.  Then in the same folder create a file called `demonoidPW.php` and place the following in that file:
+
+    <?php
+      $postInfo = array(
+        'nickname' => 'Demonoid Username',
+        'password' => 'Demonoid Password'
+      );
+    ?>
+
+## Downloading Torrents ##
+
+Normally, a user's RSS feed is `http://www.demonoid.me/rss/users/USERNAME.xml`.  Instead of using that URL, use `http://YOURSERVER/path/to/demonoid.php/USERNAME`.  The links will be changed from the torrent page (`http://www.demonoid.me/files/details/xxx/yyy`) to this script (`http://YOURSERVER/path/to/demonoid.php/xxx/yyy`).  These links will be direct links to the torrent files (the PHP script will login to demonoid, and stream the torrent file).
+
+  [1]: http://demonoid.me
---
+++ demonoid.php
@@ -0,0 +1,67 @@
+<?php if(empty($_SERVER["PATH_INFO"])) exit;
+	$get = explode('/', trim($_SERVER["PATH_INFO"],'/'));
+	if(count($get) == 2){
+		$cookie = '/tmp/cookies.txt';
+		@unlink($cookie);
+		header('Content-type: application/x-bittorrent');
+		// Link is in this format:
+		// http://www.demonoid.me/files/details/xxx/yyy
+		$details = "/files/details/{$get[0]}/{$get[1]}/";
+		$page = new DOMDocument();
+		@$page->loadHTML(file_get_contents("http://www.demonoid.me$details"));
+		$XPath = new DOMXPath($page);
+		$tables = $XPath->query('//table/tr/td[@class="ctable_content"]/a');
+		$torrent = NULL;
+		for($i = 0; $i < $tables->length; $i++){
+			// Torrent links are:
+			// http://www.demonoid.me/files/download/xxx/yyy
+			$href = $tables->item($i)->getAttribute('href');
+			if(strpos($href, '/files/download/') !== FALSE){
+				$torrent = $href;
+				break;
+			}
+		}
+		if(!is_null($torrent)){
+			// POST to https://www.demonoid.me/account_handler.php
+			require_once('demonoidPW.php');
+			$postInfo = array_merge($postInfo, array(
+				'Submit' => 'Submit',
+				'returnpath' => $torrent,
+				'withq' => 0
+			));
+
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, 'https://www.demonoid.me/account_handler.php');
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $postInfo);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+			curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie);
+			curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie);
+
+			curl_exec($ch);
+			curl_close($ch);
+			exit;
+		}
+	}
+	else{
+		header('Content-type: application/rss+xml');
+		$rss = new DOMDocument();
+		$rss->load("http://www.demonoid.me/rss/users/{$get[0]}.xml");
+		$items = $rss->getElementsByTagName('item');
+		for($i = 0; $i < $items->length; $i++){
+			$item = $items->item($i);
+			$node = $item->getElementsByTagName('link')->item(0);
+			// Link is in this format:
+			// http://www.demonoid.me/files/details/xxx/yyy
+			$link = $node->textContent;
+			$link = explode('/', rtrim($link, '/'));
+			$linkLength = count($link);
+			$id2 = $link[$linkLength-1];
+			$id1 = $link[$linkLength-2];
+			$newLink = $rss->createElement('link',
+				"http://{$_SERVER["HTTP_HOST"]}{$_SERVER["SCRIPT_NAME"]}/$id1/$id2");
+			$item->replaceChild($newLink, $node);
+		}
+		die($rss->saveXML());
+	}
+?>
